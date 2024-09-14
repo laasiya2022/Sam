2a8a06f7c9b55971edca5d02f1b9c882f05bc681
day <laasiya.sivanandam@gmail.com>
2024-09-14 09:38:47 +0530

    started

---
+++ src/test/java/Sample.java
@@ -0,0 +1,14 @@
+
+public class Sample {
+	private void method1() {
+		System.out.println("kkkk");
+
+	}
+	private void method2() {
+		System.out.println("kkkk");
+	}
+	private void method3() {
+		System.out.println("kkkk");
+	}
+
+}
