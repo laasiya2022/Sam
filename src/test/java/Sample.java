7185a8375eb6a094630642a74901c57b43199847
day <laasiya.sivanandam@gmail.com>
2024-09-14 09:51:43 +0530

    message

--- src/test/java/Sample.java
+++ src/test/java/Sample.java
@@ -10,5 +10,13 @@ public class Sample {
 	private void method3() {
 		System.out.println("kkkk");
 	}
-
+	private void method7() {
+		System.out.println("kkkk");
+	}
+	private void method8() {
+		System.out.println("kkkk");
+	}
+	private void method9() {
+		System.out.println("kkkk");
+	}
 }
